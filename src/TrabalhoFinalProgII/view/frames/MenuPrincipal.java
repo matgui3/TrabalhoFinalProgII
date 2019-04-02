09b04c9f41de075a14bfc2d341f6e8b478904903
Rodrigo <tazzsoni@gmail.com>
2019-04-02 05:21:35 -0300

    Criado Pergunta para sair do sistema e item logout no menu ajuda

--- src/TrabalhoFinalProgII/view/frames/MenuPrincipal.java
+++ src/TrabalhoFinalProgII/view/frames/MenuPrincipal.java
@@ -18,23 +18,25 @@ import javax.swing.plaf.RootPaneUI;
  *
  * @author 00783962045
  */
-public class MenuPrincipal extends JMenuBar implements ActionListener{
+public class MenuPrincipal extends JMenuBar implements ActionListener {
+
     private JMenu menuBibliotecas;
     private JMenu menuOperadores;
     private JMenu menuAjuda;
-    
+
     private JMenuItem menuItemAddFrase;
     private JMenuItem menuItemAddAjuste;
     private JMenuItem menuItemAddOperadores;
     private JMenuItem menuItemVerOperadores;
     private JMenuItem menuItemSobre;
     private JMenuItem menuItemSair;
-    
+    private JMenuItem menuItemLogOut;
+
     private FramePrincipal frameSistema;
 
     public MenuPrincipal(FramePrincipal frameSistema) {
         this.frameSistema = frameSistema;
-    
+
         initializeComponents();
         addComponents();
         addListener();
@@ -44,26 +46,29 @@ public class MenuPrincipal extends JMenuBar implements ActionListener{
         menuBibliotecas = new JMenu("Bibliotecas");
         menuOperadores = new JMenu("Operadores");
         menuAjuda = new JMenu("Ajuda");
-        
+
         menuItemAddFrase = new JMenuItem("Adicionar Frase à Biblioteca");
         menuItemAddAjuste = new JMenuItem("Adicionar Ajuste à Biblioteca");
         menuItemAddOperadores = new JMenuItem("Adicionar Operadores");
         menuItemVerOperadores = new JMenuItem("Operadores Cadastrados");
         menuItemSobre = new JMenuItem("Sobre a Aplicação");
         menuItemSair = new JMenuItem("Sair");
+        menuItemLogOut = new JMenuItem("Efetuar logout");
+        menuItemLogOut.addActionListener(this);
         menuItemSair.addActionListener(this);
     }
 
     private void addComponents() {
         menuBibliotecas.add(menuItemAddFrase);
         menuBibliotecas.add(menuItemAddAjuste);
-        
+
         menuOperadores.add(menuItemVerOperadores);
         menuOperadores.add(menuItemAddOperadores);
-        
+
         menuAjuda.add(menuItemSobre);
+        menuAjuda.add(menuItemLogOut);
         menuAjuda.add(menuItemSair);
-        
+
         super.add(menuBibliotecas);
         super.add(menuOperadores);
         super.add(menuAjuda);
@@ -78,13 +83,13 @@ public class MenuPrincipal extends JMenuBar implements ActionListener{
     public void actionPerformed(ActionEvent evt) {
         Object obj = evt.getSource();
         if (obj == menuItemSair) {
-            JOptionPane telaSaida = new JOptionPane();
+            Object[] options = {"Sim", "Não"};
+            int opcao = JOptionPane.showOptionDialog(null, "Deseja realmente sair do sistema?", 
+                    "Informação", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+            if (opcao == 0 ) { 
+                frameSistema.dispose();
+                  } 
         }
     }
-    
-    
-    
-    
-    
-    
+
 }
