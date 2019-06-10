9513c9bfad9d5d746f210f40f796fc51f836be07
Guilherme Deschamps <guilhermerd6@hotmail.com>
2019-06-09 23:02:28 -0300

    Criada classe Service p/ Ajustes, e método de criar ajustes.

---
+++ src/TrabalhoFinalProgII/service/AjustesService.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrabalhoFinalProgII.service;
+
+import TrabalhoFinalProgII.core.JPADAO;
+import TrabalhoFinalProgII.model.ValoresAlarmesTrip;
+
+/**
+ *
+ * @author guilh
+ */
+public class AjustesService {
+    
+    JPADAO<ValoresAlarmesTrip> dao = new JPADAO();
+    
+    public ValoresAlarmesTrip criarAjuste(String nomeAjuste, String valorAjusteUg1, String valorAjusteUg2) throws Exception{
+        if(nomeAjuste == null | valorAjusteUg1 == null | valorAjusteUg2 == null)
+            throw new Exception("Preencha todos os campos do formulário e tente novamente.");
+        
+        ValoresAlarmesTrip ajuste = new ValoresAlarmesTrip();
+        ajuste.setNomeAjuste(nomeAjuste);
+        ajuste.setValorAjusteUg1(valorAjusteUg1);
+        ajuste.setValorAjusteUg2(valorAjusteUg2);
+        
+        dao.inserir(ajuste);
+        
+        return ajuste;
+    }
+    
+}
