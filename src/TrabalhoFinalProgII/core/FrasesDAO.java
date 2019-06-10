01dfbd36538e2c56b7c080d7776aa3f7d166e043
Guilherme Deschamps <guilhermerd6@hotmail.com>
2019-06-09 23:54:08 -0300

    Corrigido método CriarFrase e implementado o BuscarFrases.
    No método CriarFrase faltava inserir a frase no banco de dados. Apagado também o FrasesProntasDAO.

--- src/TrabalhoFinalProgII/core/FrasesDAO.java
+++ src/TrabalhoFinalProgII/core/FrasesDAO.java
@@ -5,10 +5,23 @@
  */
 package TrabalhoFinalProgII.core;
 
+import TrabalhoFinalProgII.model.Frase;
+import java.util.List;
+import javax.persistence.Query;
+
 /**
  *
  * @author guilh
  */
-public class FrasesDAO {
+public class FrasesDAO extends JPADAO<Frase>{
+    
+    public List<Frase> buscarFrases(){
+        List<Frase> frases;
+        
+        Query query = em.createNamedQuery("Frase.buscarFrases");
+        frases = query.getResultList();
+        
+        return frases;
+    }
     
 }
