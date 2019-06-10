dd3e75fad015f70a36f8d4a0968b7e8fe5b3cda6
Guilherme Deschamps <guilhermerd6@hotmail.com>
2019-06-09 22:35:38 -0300

    Implementado o método de criação de nova frase.

--- src/TrabalhoFinalProgII/service/FrasesProntasService.java
+++ src/TrabalhoFinalProgII/service/FrasesProntasService.java
@@ -5,10 +5,26 @@
  */
 package TrabalhoFinalProgII.service;
 
+import TrabalhoFinalProgII.core.JPADAO;
+import TrabalhoFinalProgII.model.Frase;
+
 /**
  *
  * @author guilh
  */
 public class FrasesProntasService {
-    
+
+    JPADAO<Frase> dao = new JPADAO();
+
+    public Frase criarFrase(String texto, String area) throws NoSuchFieldException {
+        Frase frase = new Frase();
+
+        frase.setTexto(texto);
+        if (!area.equalsIgnoreCase("UG1") & !area.equalsIgnoreCase("UG2") & !area.equalsIgnoreCase("Serviços Auxiliares") & !area.equalsIgnoreCase("Tomada D'Água") & !area.equalsIgnoreCase("Subestação")) 
+            throw new NoSuchFieldException("Área inválida, tente novamente!");
+        else
+            frase.setArea(area);
+        
+        return frase;
+    }
 }
